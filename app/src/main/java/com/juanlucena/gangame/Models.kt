be60427fb953dd6646999ed15b5d7bd422f1ec6c
Juan Lucena <juangarcialucena@gmail.com>
2018-05-07 22:45:02 +0200

    Sección 7

--- app/src/main/java/com/juanlucena/gangame/Models.kt
+++ app/src/main/java/com/juanlucena/gangame/Models.kt
@@ -1,9 +1,37 @@
 package com.juanlucena.gangame
 
-/**
- * Created by juanantonio.garcia on 04/05/2018.
- */
 
-data class Deal(var title : String, var salePrice : Float, var normalPrice : Float, var metacriticScore : Int, var steamRating : Int, var thumb : String)
+object PriceFormatter {
 
-data class TopGame(var title : String, var owners : Int, var steamRating: Int, var publisher : String, var price : Float, var position : Int, var thumb : String)
+    val FORMAT_PRICE = "$ %.2f"
+
+    fun priceFormatted(price: Float) = String.format(FORMAT_PRICE, price)
+
+}
+
+data class Deal(var title : String,
+                var salePrice : Float,
+                var normalPrice : Float,
+                var metacriticScore : Int,
+                var steamRating : Int,
+                var thumb : String){
+
+    val salePriceFormatted : String
+    get() = PriceFormatter.priceFormatted(salePrice)
+
+    val normalPriceFormatted : String
+    get() = PriceFormatter.priceFormatted(normalPrice)
+}
+
+data class TopGame(var title : String,
+                   var owners : Int,
+                   var steamRating: Int,
+                   var publisher : String,
+                   var price : Float,
+                   var position : Int,
+                   var thumb : String){
+
+
+    val priceFormatted : String
+        get() = PriceFormatter.priceFormatted(price)
+}
