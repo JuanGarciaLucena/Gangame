08c82248a2a512e394aacc400403dac3371b2449
juanantonio.garcia <juanantonio.garcia@sdos.es>
2018-05-08 11:05:26 +0200

    no message

--- app/src/main/java/com/juanlucena/gangame/deals/DealsFragment.kt
+++ app/src/main/java/com/juanlucena/gangame/deals/DealsFragment.kt
@@ -1,14 +1,40 @@
 package com.juanlucena.gangame.deals
 
-import com.juanlucena.common.BaseFragment
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.juanlucena.common.BaseListFragment
+import com.juanlucena.common.DataBindingRecyclerAdapter
+import com.juanlucena.gangame.BR
+import com.juanlucena.gangame.Deal
 import com.juanlucena.gangame.R
 
 /**
  * Created by juanantonio.garcia on 03/05/2018.
  */
-class DealsFragment : BaseFragment(){
+class DealsFragment : BaseListFragment(){
+
+    override fun getAdapter(): RecyclerView.Adapter<*> {
+        return DataBindingRecyclerAdapter<Deal>(BR.deal, R.layout.item_deal)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Casting con la palabra reservada as
+        (listAdapter as DataBindingRecyclerAdapter<Deal>).items.addAll(getDummyDeals())
+    }
 
     override fun getLayoutResId(): Int {
         return R.layout.fragment_deals
+    }
+
+    fun getDummyDeals() : ArrayList<Deal>{
+        return arrayListOf(Deal("Counter Strike",
+                0.99F,
+                9.99F,
+                80,
+                80,
+                "http://cdn.akamai.steamstatic.com/steam/apps/10/capsule_184x69.jpg"))
     }
 }
