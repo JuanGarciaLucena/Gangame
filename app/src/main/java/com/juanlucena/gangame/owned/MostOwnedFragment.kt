b6a453adb6435bcf2ca504702268bf29cb17d120
juanantonio.garcia <juanantonio.garcia@sdos.es>
2018-05-08 10:12:50 +0200

    no message

---
+++ app/src/main/java/com/juanlucena/gangame/owned/MostOwnedFragment.kt
@@ -0,0 +1,41 @@
+package com.juanlucena.gangame.owned
+
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.juanlucena.common.BaseListFragment
+import com.juanlucena.common.DataBindingRecyclerAdapter
+import com.juanlucena.gangame.BR
+import com.juanlucena.gangame.R
+import com.juanlucena.gangame.TopGame
+
+/**
+ * Created by juanantonio.garcia on 03/05/2018.
+ */
+class MostOwnedFragment : BaseListFragment(){
+
+    override fun getAdapter(): RecyclerView.Adapter<*> {
+        return DataBindingRecyclerAdapter<TopGame>(BR.topGame, R.layout.item_top_game)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Casting con la palabra reservada as
+        (listAdapter as DataBindingRecyclerAdapter<TopGame>).items.addAll(getDummyTopGame())
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_top_owned
+    }
+
+    fun getDummyTopGame() : ArrayList<TopGame>{
+        return arrayListOf(TopGame("Counter Strike",
+                13407338,
+                80,
+                "Valve",
+                9.99F,
+                1,
+                "http://cdn.akamai.steamstatic.com/steam/apps/10/capsule_184x69.jpg"))
+    }
+}
